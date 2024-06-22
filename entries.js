15ce8038268fa54059e722df8c9fc42206f2f5d4
yannick <yannick.michallet@veolia.com>
2024-06-22 11:53:09 +0200

    add search feature + search through ?search=XXX query param

---
+++ entries.js
@@ -0,0 +1,157 @@
+export const entries = [{
+    id: 'optimization',
+    href: './optimization/',
+    name: 'Optimization algorithms',
+    desc: 'Gradient descent, Momentum, Adagrad, ...',
+    chips: [{
+        type: 'draft',
+        text: "DRAFT"
+    }]
+},{
+    id: 'logic-gates',
+    href: './logic-gates/',
+    name: 'Logic gates',
+    desc: 'NAND, Mux, Full-Adder, Memory, Program Counter, ALU, ...',
+    classes: ['disabled-entry'],
+    chips: [{
+        type: 'early-draft',
+        text: "EARLY DRAFT"
+    }]
+},{
+    id: 'splines',
+    href: './splines/',
+    name: 'Splines',
+    desc: 'Bézier curves, Bernstein polynomials, ...',
+    chips: [{
+        type: 'draft',
+        text: "DRAFT"
+    }]
+},{
+    id: 'snake',
+    href: './snake/',
+    name: 'Snake game',
+    desc: 'neural network (DAG) + genetic algorithm',
+    chips: [{
+        type: 'warning',
+        text: "WIP"
+    },{
+        type: 'buggy',
+        text: 'buggy'
+    }]
+},{
+    id: 'spreadsheet',
+    href: './spreadsheet/',
+    name: 'Spreadsheet',
+    desc: 'application of topological sort.',
+    chips: [{
+        type: 'draft',
+        text: 'DRAFT'
+    }]
+},{
+    id: 'gray-scott',
+    href: './gray-scott/',
+    name: 'Gray-Scott model',
+    desc: 'reaction-diffusion model.',
+    chips: [{
+        type: 'info',
+        text: 'slow'
+    }]
+},{
+    id: 'marbling',
+    href: './marbling/',
+    name: 'Marbling',
+    desc: 'simulate painting drops technique.',
+},{
+    id: 'lagrange',
+    href: './lagrange-interp/',
+    name: 'Lagrange interpolation',
+    desc: 'draw a polynomial interpolation curve between n given points.',
+},{
+    id: 'correlations',
+    href: './correlations/',
+    name: 'Correlation coefficients',
+    desc: "Pearson's r, Spearman's ρ, Kendall's τ, cosine similarity",
+},{
+    id: 'delaunay',
+    href: './delaunay/',
+    name: 'Delaunay triangulation',
+    desc: "method that maximizes triangles' minimal angle over a set of points.",
+    chips: [{
+        type: 'info',
+        text: 'slow'
+    }]
+},{
+    id: 'newton-fractals',
+    href: './newton-fractals/dist/newton-fractals/browser/',
+    name: 'Newton fractals',
+    desc: "based on Newton-Raphson root finding method (over a complex function).",
+    chips: [{
+        type: 'info',
+        text: 'slow startup'
+    }]
+},{
+    id: 'lyapunov',
+    href: './lyapunov-fractals/',
+    name: 'Markus-Lyapunov fractals',
+    desc: "using Lyapunov exponent λ.",
+    chips: [{
+        type: 'info',
+        text: 'slow startup'
+    }]
+},{
+    id: 'minimax',
+    href: './minimax/dist/minimax/browser/',
+    name: 'Minimax algorithm (with α-β pruning)',
+    desc: "applied to Tic-tac-toe & Connect4 games.",
+    chips: [{
+        type: 'info',
+        text: 'slow'
+    },{
+        type: 'buggy',
+        text: 'buggy'
+    }]
+},{
+    id: 'metaballs',
+    href: './metaballs/',
+    name: 'Metaballs',
+    desc: 'moving "blob" shapes using implicit function, marching square algorithm and linear interpolation.',
+    chips: [{
+        type: 'buggy',
+        text: 'buggy'
+    }]
+},{
+    id: 'shading',
+    href: './shading-methods/',
+    name: 'Shading methods (WebGL)',
+    desc: "flat, Gouraud and Phong shading.",
+    chips: [{
+        type: 'warning',
+        text: 'WIP'
+    },{
+        type: 'buggy',
+        text: 'buggy'
+    }]
+},{
+    id: 'sph',
+    href: './sph/',
+    name: 'Smoothed-particle hydrodynamics',
+    desc: "to simulate water with SPH.",
+    chips: [{
+        type: 'warning',
+        text: 'WIP'
+    }]
+},{
+    id: 'traffic',
+    href: './traffic/public/',
+    name: 'Traffic jam',
+    desc: "to control traffic lights.",
+    chips: [{
+        type: 'warning',
+        text: 'WIP'
+    }]
+},{
+    id: 'douglas-peucker',
+    href: './douglas-pucker/',
+    name: 'Douglas-Pucker algorithm (1973)',
+    desc: "path simplification, ...",
+}];
---
+++ index.js
@@ -0,0 +1,156 @@
+import { entries } from './entries.js';
+
+let currentHash = '', // current demo selected
+    searchText = '';
+
+const searchInputEl = document.querySelector("#search");
+
+window.onload = function() {
+    updateFilteredEntries(entries); // load initial list (everything !)
+
+    // handle the initial hash if needed (to click on the wanted tool)
+    onHashChange();
+    console.log("initial hash:", currentHash);
+
+    searchInputEl.focus(); // ready to search something !
+
+    if (window.location.search) {
+        const searchParam = new URLSearchParams(window.location.search.substring(1));
+        const initialSearch = searchParam.get('search');
+        if (initialSearch) {
+            searchInputEl.value = initialSearch;
+            search(initialSearch);
+
+            const visibles = visibleEntries(entries);
+            if(visibles.length === 1) { // if only 1 visible -> run it
+                //goTo(visibles[0].id);
+                const aLink = document.querySelector('#' + visibles[0].id);
+                aLink?.click();
+
+                //window.location.search = ''; // it reload the page
+
+                const url = new URL(window.location)
+                url.searchParams.delete("search");
+                history.pushState(null, null, url); // '#' + visibles[0].id
+            }
+        }
+    }
+}
+
+function onHashChange() {
+    //console.log("hash changed to", window.location.hash);
+    if(window.location.hash) { // changed to this
+        const aLink = document.querySelector(window.location.hash);
+        aLink?.click(); // click on demo
+        //document.querySelector("iframe").focus();
+        changeSelected(currentHash, window.location.hash);
+        //currentHash = window.location.hash;
+    }
+}
+
+window.addEventListener('hashchange', onHashChange, false);
+
+searchInputEl.addEventListener('input', (e) => {
+    search(e.target.value);
+});
+
+function search(text) {
+    searchText = text;
+    updateFilteredEntries(entries, searchText);
+}
+
+searchInputEl.addEventListener('keyup', (e) => {
+    if(e.code === 'Enter') {
+        const visibles = visibleEntries(entries);
+        if(visibles.length === 1) { // if only 1 visible -> run it
+            //goTo(visibles[0].id);
+            const aLink = document.querySelector('#' + visibles[0].id);
+            aLink?.click();
+        }
+    }
+});
+
+const menu = document.querySelector("#menu");
+menu.addEventListener('click', (e) => {
+    //console.log("e:", e);
+    //console.log("clicked on ", e.target.id, e.target.tagName)
+
+    if(e.target.tagName === 'A' && e.target.id) {
+        console.log("clicked on a demo :", e.target.id, e);
+       goTo(e.target.id);
+    }
+});
+
+function goTo(id) {
+    console.log("goto :", id);
+
+    //const aLink = document.querySelector("#" + id);
+    //aLink?.click(); // click on demo
+
+    history.pushState(null, null, '#' + id); // ???
+
+    // from, to
+    changeSelected(currentHash, '#' + id); // change color to see which tool is selected
+
+    // change to new id
+    currentHash = '#' + id;
+    console.log("new currentHash =", currentHash);
+}
+
+function changeSelected(fromHash, toHash) {
+    //console.log("from", fromHash, "to", toHash);
+    if(fromHash) {
+        const from = document.querySelector(fromHash);
+        if(from) {
+            from.parentNode.classList.remove('highlighted-demo');
+        }
+    }
+    if(toHash) {
+        const to = document.querySelector(toHash);
+        if(to) {
+            to.parentNode.classList.add('highlighted-demo');
+            to.parentNode.scrollIntoView(false);
+        }
+    }
+}
+
+function addMenuEntry(entry) {
+    const chips = entry.chips?.map(chip => `<span class="chip ${chip.type}">${chip.text}</span>`);
+
+    if(entry.id === currentHash.substring(1)) {
+        console.log("matching :", entry.id)
+        if(Array.isArray(entry.classes)) {
+            entry.classes.push('highlighted-demo');
+        } else {
+            entry.classes = ['highlighted-demo'];
+        }
+    } else {
+        if(Array.isArray(entry.classes)) {
+            entry.classes = entry.classes.filter(c => c !== 'highlighted-demo');
+        }
+    }
+
+    const html = `<span${entry.classes ? ' class="' + entry.classes.join(' ') + '"' : ''}>
+        <a id="${entry.id}" href="${entry.href}" target="iframe">${entry.name}</a> ${chips?.join(' ') ?? ''}<br/>
+        <span class="desc">${entry.desc}</span>
+    </span>`;
+
+    const container = document.createElement('container');
+    container.innerHTML = html;
+    document.querySelector("#menu-entries").appendChild(container.firstChild);
+}
+
+function updateFilteredEntries(entries, searchText = '') {
+    document.querySelector("#menu-entries").innerHTML = ''; // clear !!
+    entries.filter(entry => {
+        return entry.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            entry.desc.toLowerCase().includes(searchText.toLowerCase());
+    }).forEach(entry => addMenuEntry(entry));
+}
+
+function visibleEntries(entries) {
+    return entries.filter(entry => {
+        return entry.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            entry.desc.toLowerCase().includes(searchText.toLowerCase());
+    });
+}
