7380e98ad3d3e75848c449eba844fa1edc479441
yannick <yannick.michallet@veolia.com>
2024-06-30 11:08:58 +0200

    feat(book-progression): add visibility

---
+++ book-progression/SICP.js
@@ -0,0 +1,190 @@
+export const SICP = {
+    title: 'Structure and Interpretation of Computer Programs, 2nd ed. (1996)',
+    nb_pages: 855,
+    id: 'SICP',
+    content: [{
+        title: '1 - Building Abstractions with Procedures',
+        start_page: 1,
+        content: [{
+            title: '1.1 - The Elements of Programming',
+            start_page: 6,
+            content: [{
+                id: '1.1.1',
+                title: '1.1.1 - Expressions',
+                start_page: 7
+            },{
+                id: '1.1.2',
+                title: '1.1.2 - Naming and the Environment',
+                start_page: 10
+            },{
+                id: '1.1.3',
+                title: '1.1.3 - Evaluating Combinations',
+                start_page: 12
+            },{
+                id: '1.1.4',
+                title: '1.1.4 - Compound Procedures',
+                start_page: 15
+            },{
+                id: '1.1.5',
+                title: '1.1.5 - The Substitution Model for Procedure Application',
+                start_page: 18
+            },{
+                id: '1.1.6',
+                title: '1.1.6 - Conditional Expressions and Predicates',
+                start_page: 22
+            },{
+                id: '1.1.7',
+                title: '1.1.7 - Example: Square Roots by Newton\'s Method',
+                start_page: 28
+            },{
+                id: '1.1.8',
+                title: '1.1.8 - Procedures as Black-Box Abstractions',
+                start_page: 33
+            }]
+        },{
+            title: '1.2 - Procedures and the Processes They Generate',
+            start_page: 40,
+            content: [{
+                id: '1.2.1',
+                title: '1.2.1 - Linear Recursion and Iteration',
+                start_page: 41
+            },{
+                id: '1.2.2',
+                title: '1.2.2 - Tree recursion',
+                start_page: 47
+            },{
+                id: '1.2.3',
+                title: '1.2.3 - Orders of Growth',
+                start_page: 54
+            },{
+                id: '1.2.4',
+                title: '1.2.4 - Exponentiation',
+                start_page: 57
+            },{
+                id: '1.2.5',
+                title: '1.2.5 - Greatest Common Divisors',
+                start_page: 62
+            },{
+                id: '1.2.6',
+                title: '1.2.6 - Example: Testing for Primality',
+                start_page: 65
+            }]
+        },{
+            title: '1.3 - Formulating Abstractions with Higher-Order Procedures',
+            start_page: 74,
+            content: [{
+                id: '1.3.1',
+                title: '1.3.1 - Procedures as Arguments',
+                start_page: 76
+            },{
+                id: '1.3.2',
+                title: '1.3.2 - Constructing Procedures using lambda',
+                start_page: 83
+            },{
+                id: '1.3.3',
+                title: '1.3.3 - Procedures as General Methods',
+                start_page: 89
+            },{
+                id: '1.3.4',
+                title: '1.3.4 - Procedures as Returned Values',
+                start_page: 97
+            }]
+        }]
+    },{
+        title: '2 - Building Abstractions with Data',
+        start_page: 107,
+        content: [{
+            title: '2.1 - Introduction to Data Abstraction',
+            start_page: 112,
+            content: [{
+                id: '2.1.1',
+                title: '2.1.1 - Example: Arithmetic Operations for Rational Numbers',
+                start_page: 113
+            },{
+                id: '2.1.2',
+                title: '2.1.2 - Abstraction Barriers',
+                start_page: 118
+            },{
+                id: '2.1.3',
+                title: '2.1.3 - What is Meant by Data?',
+                start_page: 122
+            },{
+                id: '2.1.4',
+                title: '2.1.4 - Extended Exercice: Interval Arithmetic',
+                start_page: 126
+            }]
+        },{
+            title: '2.2 - Hierarchical Data and the Closure Property',
+            start_page: 132,
+            content: [{
+                id: '2.2.1',
+                title: '2.2.1 - Representing Sequences',
+                start_page: 134
+            },{
+                id: '2.2.2',
+                title: '2.2.2 - Hierarchical Structures',
+                start_page: 147
+            },{
+                id: '2.2.3',
+                title: '2.2.3 - Sequences as Conventional Interfaces',
+                start_page: 154
+            },{
+                id: '2.2.4',
+                title: '2.2.4 - Example: A Picture Language',
+                start_page: 172
+            }]
+        },{
+            title: '2.3 - Symbolic Data',
+            start_page: 192,
+            content: [{
+                id: '2.3.1',
+                title: '2.3.1 - Quotation',
+                start_page: 192
+            },{
+                id: '2.3.2',
+                title: '2.3.2 - Example: Symbolic Differentiation',
+                start_page: 197
+            },{
+                id: '2.3.3',
+                title: '2.3.3 - Example: Representing Sets',
+                start_page: 205
+            },{
+                id: '2.3.4',
+                title: '2.3.4 - Example: Huffman Encoding Trees',
+                start_page: 218
+            }]
+        },{
+            title: '2.4 - Multiple Representation for Abstract Data',
+            start_page: 229,
+            content: [{
+                id: '2.4.1',
+                title: '2.4.1 - Representations for Complex Numbers',
+                start_page: 232
+            },{
+                id: '2.4.2',
+                title: '2.4.2 - Tagged data',
+                start_page: 237
+            },{
+                id: '2.4.3',
+                title: '2.4.3 - Data-Directed Progamming and Additivity',
+                start_page: 242
+            }]
+        },{
+            title: '2.5 - Systems with Generic Operations',
+            start_page: 254,
+            content: [{
+                id: '2.5.1',
+                title: '2.5.1 - Generic Arithmetic Operations',
+                start_page: 255
+            },{
+                id: '2.5.2',
+                title: '2.5.2 - Combining Data of Different Types',
+                start_page: 262
+            },{
+                id: '2.5.3',
+                title: '2.5.3 - Example: Symbolic Algebra',
+                start_page: 274
+            }]
+        }]
+    }]
+};
--- book-progression/book-progression.js
+++ book-progression/book-progression.js
@@ -2,7 +2,11 @@
 import { setUpCanvas, drawPointAt, drawArrow, drawLine } from '../common/canvas.helper.js';
 import { randInt } from '../common/common.helper.js';
 
+// hard-coded books
+import { SICP } from './SICP.js';
+
 const bookListEl = document.getElementById("book_list");
+
 
 function main() {
     //document.querySelector("#refresh").addEventListener('click', (e) => redraw());
@@ -15,208 +19,21 @@ function main() {
  * TODO: get it from localStorage !
  */
 function getBookList() {
-    return [{
-        title: 'Structure and Interpretation of Computer Programs, 2nd ed. (1996)',
-        nb_pages: 855,
-        id: 'SICP',
-        content: [{
-            title: '1 - Building Abstractions with Procedures',
-            start_page: 1,
-            content: [{
-                title: '1.1 - The Elements of Programming',
-                start_page: 6,
-                content: [{
-                    id: '1.1.1',
-                    title: '1.1.1 - Expressions',
-                    start_page: 7
-                },{
-                    id: '1.1.2',
-                    title: '1.1.2 - Naming and the Environment',
-                    start_page: 10
-                },{
-                    id: '1.1.3',
-                    title: '1.1.3 - Evaluating Combinations',
-                    start_page: 12
-                },{
-                    id: '1.1.4',
-                    title: '1.1.4 - Compound Procedures',
-                    start_page: 15
-                },{
-                    id: '1.1.5',
-                    title: '1.1.5 - The Substitution Model for Procedure Application',
-                    start_page: 18
-                },{
-                    id: '1.1.6',
-                    title: '1.1.6 - Conditional Expressions and Predicates',
-                    start_page: 22
-                },{
-                    id: '1.1.7',
-                    title: '1.1.7 - Example: Square Roots by Newton\'s Method',
-                    start_page: 28
-                },{
-                    id: '1.1.8',
-                    title: '1.1.8 - Procedures as Black-Box Abstractions',
-                    start_page: 33
-                }]
-            },{
-                title: '1.2 - Procedures and the Processes They Generate',
-                start_page: 40,
-                content: [{
-                    id: '1.2.1',
-                    title: '1.2.1 - Linear Recursion and Iteration',
-                    start_page: 41
-                },{
-                    id: '1.2.2',
-                    title: '1.2.2 - Tree recursion',
-                    start_page: 47
-                },{
-                    id: '1.2.3',
-                    title: '1.2.3 - Orders of Growth',
-                    start_page: 54
-                },{
-                    id: '1.2.4',
-                    title: '1.2.4 - Exponentiation',
-                    start_page: 57
-                },{
-                    id: '1.2.5',
-                    title: '1.2.5 - Greatest Common Divisors',
-                    start_page: 62
-                },{
-                    id: '1.2.6',
-                    title: '1.2.6 - Example: Testing for Primality',
-                    start_page: 65
-                }]
-            },{
-                title: '1.3 - Formulating Abstractions with Higher-Order Procedures',
-                start_page: 74,
-                content: [{
-                    id: '1.3.1',
-                    title: '1.3.1 - Procedures as Arguments',
-                    start_page: 76
-                },{
-                    id: '1.3.2',
-                    title: '1.3.2 - Constructing Procedures using lambda',
-                    start_page: 83
-                },{
-                    id: '1.3.3',
-                    title: '1.3.3 - Procedures as General Methods',
-                    start_page: 89
-                },{
-                    id: '1.3.4',
-                    title: '1.3.4 - Procedures as Returned Values',
-                    start_page: 97
-                }]
-            }]
-        },{
-            title: '2 - Building Abstractions with Data',
-            start_page: 107,
-            content: [{
-                title: '2.1 - Introduction to Data Abstraction',
-                start_page: 112,
-                content: [{
-                    id: '2.1.1',
-                    title: '2.1.1 - Example: Arithmetic Operations for Rational Numbers',
-                    start_page: 113
-                },{
-                    id: '2.1.2',
-                    title: '2.1.2 - Abstraction Barriers',
-                    start_page: 118
-                },{
-                    id: '2.1.3',
-                    title: '2.1.3 - What is Meant by Data?',
-                    start_page: 122
-                },{
-                    id: '2.1.4',
-                    title: '2.1.4 - Extended Exercice: Interval Arithmetic',
-                    start_page: 126
-                }]
-            },{
-                title: '2.2 - Hierarchical Data and the Closure Property',
-                start_page: 132,
-                content: [{
-                    id: '2.2.1',
-                    title: '2.2.1 - Representing Sequences',
-                    start_page: 134
-                },{
-                    id: '2.2.2',
-                    title: '2.2.2 - Hierarchical Structures',
-                    start_page: 147
-                },{
-                    id: '2.2.3',
-                    title: '2.2.3 - Sequences as Conventional Interfaces',
-                    start_page: 154
-                },{
-                    id: '2.2.4',
-                    title: '2.2.4 - Example: A Picture Language',
-                    start_page: 172
-                }]
-            },{
-                title: '2.3 - Symbolic Data',
-                start_page: 192,
-                content: [{
-                    id: '2.3.1',
-                    title: '2.3.1 - Quotation',
-                    start_page: 192
-                },{
-                    id: '2.3.2',
-                    title: '2.3.2 - Example: Symbolic Differentiation',
-                    start_page: 197
-                },{
-                    id: '2.3.3',
-                    title: '2.3.3 - Example: Representing Sets',
-                    start_page: 205
-                },{
-                    id: '2.3.4',
-                    title: '2.3.4 - Example: Huffman Encoding Trees',
-                    start_page: 218
-                }]
-            },{
-                title: '2.4 - Multiple Representation for Abstract Data',
-                start_page: 229,
-                content: [{
-                    id: '2.4.1',
-                    title: '2.4.1 - Representations for Complex Numbers',
-                    start_page: 232
-                },{
-                    id: '2.4.2',
-                    title: '2.4.2 - Tagged data',
-                    start_page: 237
-                },{
-                    id: '2.4.3',
-                    title: '2.4.3 - Data-Directed Progamming and Additivity',
-                    start_page: 242
-                }]
-            },{
-                title: '2.5 - Systems with Generic Operations',
-                start_page: 254,
-                content: [{
-                    id: '2.5.1',
-                    title: '2.5.1 - Generic Arithmetic Operations',
-                    start_page: 255
-                },{
-                    id: '2.5.2',
-                    title: '2.5.2 - Combining Data of Different Types',
-                    start_page: 262
-                },{
-                    id: '2.5.3',
-                    title: '2.5.3 - Example: Symbolic Algebra',
-                    start_page: 274
-                }]
-            }]
-        }]
-    }]
+    return [SICP]
 }
 
 function createHtml(book) {
     const html = `
         <div id="${book.id}" class="book">
+            <div class="toggle">${getVisibility(book.id) ? '➖' : '➕'}</div>
+
             <span class="title">${book.title}</span>${book.authors ? ` ${book.authors.join(', ')}` : ''}
 
             <div class="progress-bar">
                 <div class="progress"></div>
             </div>
 
-            <div class="level">
+            <div class="level ${getVisibility(book.id) ? '' : 'toggled'}">
                 ${createLevel(book, book.content, 0).join('')}
             </div>
         </div>
@@ -255,7 +72,7 @@ function findNextEntry(book, id) {
 }
 
 function getProgress(bookId) {
-    const bookProgress = JSON.parse(localStorage.getItem('book_progress') ?? '[]');
+    const bookProgress = _getLocalStorageObj('book_progress');
     return bookProgress.find(b => b.id === bookId) ?? {id: bookId, progress: []} ;
 }
 
@@ -265,22 +82,66 @@ function getProgressForId(bookId, id) {
 }
 
 function setProgress(bookId, newValue) {
-    const booksProgress = JSON.parse(localStorage.getItem('book_progress') ?? '[]');
+    const booksProgress = _getLocalStorageObj('book_progress');
     const newProgress = booksProgress.filter(prog => prog.id !== bookId);
     newProgress.push(newValue);
     localStorage.setItem('book_progress', JSON.stringify(newProgress)); // in LocalStorage
 }
 
+function _getLocalStorageObj(itemName) {
+    return JSON.parse(localStorage.getItem(itemName) ?? '[]');
+}
+
+function setVisibility(bookId, value) {
+    const booksProgress = _getLocalStorageObj('book_progress');
+    // get and keep unchanged books
+    const newObject = booksProgress.filter(prog => prog.id !== bookId);
+    // modify current book
+    const currentBook = booksProgress.find(prog => prog.id === bookId);
+    currentBook['visibility'] = value;
+    // save it
+    newObject.push(currentBook);
+    localStorage.setItem('book_progress', JSON.stringify(newObject)); // in LocalStorage
+    return value;
+}
+
+function getVisibility(bookId) {
+    const booksProgress = _getLocalStorageObj('book_progress');
+    return booksProgress.find(prog => prog.id === bookId)?.visibility ?? true;
+}
+
 function addEvents() {
 
-    const els = document.querySelectorAll("#book_list [class^='content_title']")
+    //
+    // handle toggle book view : list vs progress_only
+    //
+    const toggleEls = document.querySelectorAll("#book_list .toggle");
+    toggleEls.forEach(el => {
+        el.addEventListener('click', (e) => {
+            const bookId = e.target.offsetParent.id;
+            const currentVisibility = getVisibility(bookId);
 
-    els.forEach(el => {
+            const newVisibility = setVisibility(bookId, !currentVisibility);
+
+            if(newVisibility) {
+                e.target.offsetParent.querySelector('.level').classList.remove('toggled');
+                e.target.innerText = '➖';
+            } else {
+                e.target.offsetParent.querySelector('.level').classList.add('toggled');
+                e.target.innerText = '➕';
+            }
+        });
+    });
+
+
+
+    const titleEls = document.querySelectorAll("#book_list [class^='content_title']")
+    // for each book section/title ? -> tick a checkbox ! 
+    titleEls.forEach(el => {
         if(el.id) {
             el.addEventListener('click', (e) => {
 
-                const bookId = 'SICP';
-
+                const bookId = e.target.offsetParent.id; // use nearest positionned parent ? (because of position: relative ?)
                 console.log(e.target.innerText, "clicked")
 
                 const checkbox = e.target.querySelector("input[type='checkbox']");
@@ -297,21 +158,19 @@ function addEvents() {
 
                     if(book.progress.includes(e.target.id)) book.progress = book.progress.filter(id => id !== e.target.id); // remove
                 }
-                // save new progress
+                // save new progress to localStorage
                 setProgress(bookId, book);
 
-
-
-                // TODO
-                // compute progression
-                // by getting the "next" element (even if it is an other chapter !) then (next_start_page - start_page)
-                // sum everything and divide by the total pages
                 updateProgressBar(book)
             })
         }
     })
 }
 
+// TODO
+// compute "true" progression using pages
+//      by getting the "next" element (even if it is an other chapter !) then (next_start_page - start_page)
+// sum everything and divide by the total pages of the book (easy)
 function updateProgressBar(book) {
 
     function countIds(content) {
@@ -339,7 +198,7 @@ function redraw() {
     // add it to the DOM
     booksHtml.forEach(html => bookListEl.innerHTML += html);
 
-    // on click
+    // UI: on clicks, ...
     addEvents();
 
     // update initial progress
