ea7409ed24ccdf1c174a33c8b0a86d85e2897350
yannick <yannick.michallet@veolia.com>
2024-06-30 19:47:12 +0200

    feat(book-progression): add search

---
+++ book-progression/_books/CG.js
@@ -0,0 +1,227 @@
+export const CG = {
+    title: 'Computational Geometry - Algorithms & Applications, 3rd ed. (2008)',
+    nb_pages: 386,
+    id: 'CG',
+    front_cover: 'https://m.media-amazon.com/images/I/614hApBTToL._AC_UF100,100_QL80_.jpg',
+    tags: [{
+        text: 'todo',
+        bgColor: 'yellow',
+        textColor: 'black'
+    }],
+    content: [{
+        title: '1 - Computational Geometry',
+        desc: 'Introduction',
+        start_page: 1,
+        content: [{
+            id: '1.1',
+            title: '1.1 - An Example: Convex Hulls',
+            start_page: 2,
+        },{
+            id: '1.2',
+            title: '1.2 - Degeneracies and Robustness',
+            start_page: 8
+        },{
+            id: '1.3',
+            title: '1.3 - Application Domains',
+            start_page: 10
+        },{
+            id: '1.4',
+            title: '1.4 - Notes and Comments',
+            start_page: 13
+        },{
+            id: '1.5',
+            title: '1.5 - Exercices',
+            start_page: 15
+        }]
+    },{
+        title: '2 - Line Segment Intersection',
+        desc: 'Thematic Map Overlay',
+        start_page: 19,
+        content: [{
+            id: '2.1',
+            title: '2.1 - Line Segment Intersection',
+            start_page: 20
+        },{
+            id: '2.2',
+            title: '2.2 - The Doubly-Connected Edge List',
+            start_page: 29
+        },{
+            id: '2.3',
+            title: '2.3 - Computing the Overlay of Two Subdivisions',
+            start_page: 33
+        },{
+            id: '2.4',
+            title: '2.4 - Boolean Operations',
+            start_page: 39
+        },{
+            id: '2.5',
+            title: '2.5 - Notes and Comments',
+            start_page: 40
+        },{
+            id: '2.6',
+            title: '2.6 - Exercices',
+            start_page: 41
+        }]
+    },{
+        title: '3 - Polygon Triangulation',
+        desc: 'Guarding an Art Gallery',
+        start_page: 45,
+        content: [{
+            id: '3.1',
+            title: '3.1 - Guarding and Triangulations',
+            start_page: 46,
+        },{
+            id: '3.2',
+            title: '3.2 - Partitioning a Polygon into Monotone Pieces',
+            start_page: 49
+        },{
+            id: '3.3',
+            title: '3.3 - Triangulating a Monotone Polygon',
+            start_page: 55
+        },{
+            id: '3.4',
+            title: '3.4 - Notes and Comments',
+            start_page: 59
+        },{
+            id: '3.5',
+            title: '3.5 - Exercices',
+            start_page: 60
+        }]
+    },{
+        title: '4 - Linear Programming',
+        desc: 'Manufacturing with molds',
+        start_page: 63,
+        content: [{
+            id: '4.1',
+            title: '4.1 - The Geometry of Casting',
+            start_page: 64,
+        },{
+            id: '4.2',
+            title: '4.2 - Half-Plane Intersection',
+            start_page: 66
+        },{
+            id: '4.3',
+            title: '4.3 - Incremental Linear Programming',
+            start_page: 71
+        },{
+            id: '4.4',
+            title: '4.4 - Randomized Linear Programming',
+            start_page: 76
+        },{
+            id: '4.5',
+            title: '4.5 - Unbounded Linear Programming',
+            start_page: 79
+        },{
+            id: '4.6',
+            title: '4.6* - Linear Programming in Higher Dimension',
+            start_page: 82
+        },{
+            id: '4.7',
+            title: '4.7* - Smallest Enclosing Discs',
+            start_page: 86
+        },{
+            id: '4.8',
+            title: '4.8 - Notes and Comments',
+            start_page: 89
+        },{
+            id: '4.9',
+            title: '4.9 - Exercises',
+            start_page: 91
+        }]
+    },{
+        title: '5 - Orthogonal Range Searching',
+        desc: 'Querying a Database',
+        start_page: 95,
+        content: [{
+            id: '5.1',
+            title: '5.1 - 1-Dimensional Range Searching',
+            start_page: 96,
+        },{
+            id: '5.2',
+            title: '5.2 - Kd-Trees',
+            start_page: 99
+        },{
+            id: '5.3',
+            title: '5.3 - Range Trees',
+            start_page: 105
+        },{
+            id: '5.4',
+            title: '5.4 - Higher-Dimensional Range Trees',
+            start_page: 109
+        },{
+            id: '5.5',
+            title: '5.5 - General Sets of Points',
+            start_page: 110
+        },{
+            id: '5.6',
+            title: '5.6* - Fractional Cascading',
+            start_page: 111
+        },{
+            id: '5.7',
+            title: '5.7 - Notes and Comments',
+            start_page: 115
+        },{
+            id: '5.8',
+            title: '5.8 - Exercises',
+            start_page: 117
+        }]
+    },{
+        title: '6 - Point Location',
+        desc: 'Knowing Where You Are',
+        start_page: 121,
+        content: [{
+            id: '6.1',
+            title: '6.1 - Point Location and Trapezoidal Maps',
+            start_page: 122,
+        },{
+            id: '6.2',
+            title: '6.2 - A Randomized Incremental Algorithm',
+            start_page: 128
+        },{
+            id: '6.3',
+            title: '6.3 - Dealing with Degenerate Cases',
+            start_page: 137
+        },{
+            id: '6.4',
+            title: '6.4* - A Tail Estimate',
+            start_page: 140
+        },{
+            id: '6.5',
+            title: '6.5 - Notes and Comments',
+            start_page: 143
+        },{
+            id: '6.6',
+            title: '6.6 - Exercises',
+            start_page: 144
+        }]
+    },{
+        title: '7 - Voronoi Diagrams',
+        desc: 'The Post Office Problem',
+        start_page: 147,
+        content: [{
+            id: '7.1',
+            title: '7.1 - Definition and Basic Properties',
+            start_page: 148,
+        },{
+            id: '7.2',
+            title: '7.2 - Computing the Voronoi Diagram',
+            start_page: 151
+        },{
+            id: '7.3',
+            title: '7.3 - Voronoi Diagrams of Line Segments',
+            start_page: 160
+        },{
+            id: '7.4',
+            title: '7.4 - Farthest-Point Voronoi Diagrams',
+            start_page: 140
+        },{
+            id: '7.5',
+            title: '7.5 - Notes and Comments',
+            start_page: 143
+        },{
+            id: '7.6',
+            title: '7.6 - Exercises',
+            start_page: 144
+        }]
+    }]
+};
--- book-progression/book-progression.js
+++ book-progression/book-progression.js
@@ -4,9 +4,9 @@ import { randInt } from '../common/common.helper.js';
 import { computeBézierCurve } from '../common/math.helper.js';
 
 // hard-coded books
-import { SICP } from './SICP.js';
-import { CG } from './CG.js';;
-import { VG } from './VG.js';
+import { SICP } from './_books/SICP.js';
+import { CG } from './_books/CG.js';;
+import { VG } from './_books/VG.js';
 
 /**
  * TODO:
@@ -38,38 +38,33 @@ import { VG } from './VG.js';
 
 
 
-
-
-
-
-
-
 const bookListEl = document.getElementById("book_list");
+const searchEl = document.querySelector("input[type='search']");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-window.addEventListener("resize", (event) => updateArrows());
 
 function main() {
     //document.querySelector("#refresh").addEventListener('click', (e) => redraw());
-    redraw();
+    redraw(getBookList()); // get all books
 }
-
-
-
-const arrowsList = [
-    ['SICP', 'CG', 'solid'],
-    ['CG', 'VG', 'dashed']
-]
 
 
 
 
 /**
+ * Links
+ */
+const arrowsList = [
+    ['SICP', 'CG', 'solid'],
+    ['CG', 'VG', 'dashed']
+]
+
+/**
  * TODO: get it from localStorage too ! (or backend ?!)
  */
 function getBookList() {
-    return [SICP, CG, VG]
+    return [SICP, CG, VG];
 }
 
 function createHtml(book) {
@@ -87,7 +82,6 @@ function createHtml(book) {
                 <div style="width: 100%">
                     <div class="toggle">${getVisibility(book.id) ? '➖' : '➕'}</div>
                     ${book.tags?.length ? `<div class="tags">${book.tags?.map(tag => {
-                        console.log(tag)
                         return `<span class="tag" style="background-color: ${tag.bgColor}; color: ${tag.textColor}">${tag.text}</span>`
                     }).join('')}</div>` : ''}
 
@@ -105,20 +99,17 @@ function createHtml(book) {
             </div>
         </div>
     `;
-
     return html;
 }
 
 function createLevel(book, content, level) {
-    //const content = el.content;
-
     return content.map(el => {
         const isChecked = el.id ? getProgressForId(book.id, el.id) : false;
         return `
         <div class="content ${el.content ? '' : 'leaf'}">
 
             <span${el.id ? ' id="'+el.id+'"' : ''} class="content_title_${level}${isChecked ? ' checked' : ''}">
-                ${el.content ? '' : `<input type="checkbox" ${isChecked ? ' checked' : ''}/>`} ${el.title}
+                ${el.content ? '' : `<input type="checkbox" ${isChecked ? ' checked' : ''}/>`} ${el.title.replace(new RegExp(searchEl.value, 'ig'), (m) => '<mark>' + m + '</mark>')}
             </span>
             <span class="start_page">${el.start_page}</span>
 
@@ -129,16 +120,18 @@ function createLevel(book, content, level) {
 }
 
 
+// TODO: the compute the page number of the next element !
 function findNextEntry(book, id) {
     book.content.forEach(content => {
 
-
-
     });
-
     return nextEntry;
 }
 
+
+/**
+ * LocalStorage
+ */
 function getProgress(bookId) {
     const bookProgress = _getLocalStorageObj('book_progress');
     return bookProgress.find(b => b.id === bookId);
@@ -184,86 +177,155 @@ function getVisibility(bookId) {
     return booksProgress.find(prog => prog.id === bookId)?.visibility ?? true;
 }
 
+
+function removeEvents() {
+
+    const toggleEls = document.querySelectorAll("#book_list .toggle");
+    toggleEls.forEach(el => el.removeEventListener('click', toggleEventFn));
+
+    const titleEls = document.querySelectorAll("#book_list [class^='content_title']")
+    titleEls.forEach(el => el.id && el.removeEventListener('click', clickCheckboxEvent));
+}
+
+function clickCheckboxEvent (e) {
+
+    console.log("???");
+    
+    const bookId = e.target.offsetParent.id; // use nearest positionned parent ? (because of position: relative ?)
+    
+    const checkbox = e.target.querySelector("input[type='checkbox']");
+    checkbox.checked = !checkbox.checked; // toggle
+
+    const book = getProgress(bookId);
+    if(checkbox.checked) {
+        e.target.classList.add('checked');
+
+        if(! book.progress.includes(e.target.id)) book.progress.push(e.target.id) // add to completed ids (eg. 1.1.1, ...)
+
+    } else {
+        e.target.classList.remove('checked');
+
+        if(book.progress.includes(e.target.id)) book.progress = book.progress.filter(id => id !== e.target.id); // remove
+    }
+    // save new progress to localStorage
+    setProgress(bookId, book);
+
+    updateProgressBar(book)
+}
+
+const toggleEventFn = function(e) {
+    const bookId = e.target.offsetParent.id;
+    const currentVisibility = getVisibility(bookId);
+
+    const newVisibility = setVisibility(bookId, !currentVisibility);
+
+    if(newVisibility) {
+        e.target.offsetParent.querySelector('.level').classList.remove('toggled');
+        e.target.innerText = '➖';
+        const test = getBookList().filter(book => book.id != bookId).forEach(book => book.visibility && document.querySelector("#"+book.id+" .toggle").click());
+    } else {
+        e.target.offsetParent.querySelector('.level').classList.add('toggled');
+        e.target.innerText = '➕';
+    }
+
+    updateArrows();
+}
+
+/**
+ * Events
+ */
 function addEvents() {
+
+    window.addEventListener("resize", (event) => updateArrows());
+    window.addEventListener("scroll", (event) => updateArrows());
 
     //
     // handle toggle book view : list vs progress_only
     //
     const toggleEls = document.querySelectorAll("#book_list .toggle");
-    toggleEls.forEach(el => {
-        el.addEventListener('click', (e) => {
-            const bookId = e.target.offsetParent.id;
-            const currentVisibility = getVisibility(bookId);
-
-            const newVisibility = setVisibility(bookId, !currentVisibility);
-
-            if(newVisibility) {
-                e.target.offsetParent.querySelector('.level').classList.remove('toggled');
-                e.target.innerText = '➖';
-                const test = getBookList().filter(book => book.id != bookId).forEach(book => book.visibility && document.querySelector("#"+book.id+" .toggle").click());
-            } else {
-                e.target.offsetParent.querySelector('.level').classList.add('toggled');
-                e.target.innerText = '➕';
-            }
-
-            updateArrows();
-        });
-    });
-
+    toggleEls.forEach(el => el.addEventListener('click', toggleEventFn));
 
     const titleEls = document.querySelectorAll("#book_list [class^='content_title']")
     // for each book section/title ? -> tick a checkbox ! 
-    titleEls.forEach(el => {
-        if(el.id) {
-            el.addEventListener('click', (e) => {
-                const bookId = e.target.offsetParent.id; // use nearest positionned parent ? (because of position: relative ?)
-                
-                const checkbox = e.target.querySelector("input[type='checkbox']");
-                checkbox.checked = !checkbox.checked; // toggle
+    titleEls.forEach(el => el.id && el.addEventListener('click', clickCheckboxEvent));
 
-                const book = getProgress(bookId);
-                if(checkbox.checked) {
-                    e.target.classList.add('checked');
+    // remove ?
+    searchEl.removeEventListener('keyup', searchKeyUp);
+    searchEl.addEventListener('keyup', searchKeyUp);
 
-                    if(! book.progress.includes(e.target.id)) book.progress.push(e.target.id) // add to completed ids (eg. 1.1.1, ...)
-
-                } else {
-                    e.target.classList.remove('checked');
-
-                    if(book.progress.includes(e.target.id)) book.progress = book.progress.filter(id => id !== e.target.id); // remove
-                }
-                // save new progress to localStorage
-                setProgress(bookId, book);
-
-                updateProgressBar(book)
-            })
-        }
-    })
 }
 
-function updateArrows() {
+function searchKeyUp(e) {
 
-    console.log("update arrows");
+    // filter all the books objects (remove unnecessary leaf + if not leaf && not necessary -> remove !)
 
+    const searchStr = e.target.value;
+    console.log("searching for", searchStr)
+
+    let copy = structuredClone(getBookList());
+
+    function prune(book, content, depth) {
+        //console.log("pruning content:", content, "at depth:", depth)
+
+        //if(content.length) { // is there a deeper level ?
+            // search & prune list recursively
+            content = content.map(entry => entry.content ?
+                    prune(entry, entry.content, depth + 1) // todo ? can be not passing the test but its children yes...
+                : { id: entry.id, title: entry.title, start_page: entry.start_page }
+            ).filter(entry => entry); // remove nulls
+            
+            // list of found matchings
+            const filteredContent = content.filter(entry => {
+                const test = entry.title.toLowerCase().includes(searchStr.toLowerCase());
+                //console.log(">", entry.title, "is matching ?", test);
+                return test || content.every(entry => entry.content?.length > 0);
+            });
+
+            return filteredContent.length === 0 ?
+                null : // nothing found, this "node" will be removed, also its children as they don't have any values in them..
+                { id: book.id, title: book.title, start_page: book.start_page, content: filteredContent };
+        //}
+
+        //return; // never reached ?!
+    }
+
+    if(searchStr) {
+        copy.forEach(book => {
+            const res = book.content.map(entry => prune(entry, entry.content, 0)).filter(entry => entry); // remove nulls
+            //console.log(">>> res:", res);
+            book.content = res;
+        });
+        //console.warn("result:", copy);
+    }
+
+    removeEvents();
+    // erase all .....
+    bookListEl.innerHTML = '';
+
+    redraw(searchStr ? copy : getBookList());
+}
+
+
+function updateArrows()
+{
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     setUpCanvas(ctx, canvas.width, canvas.height, 'white')
 
     const pointSize = 5, color = "#467799";
 
-
     arrowsList.forEach(([from, to, style]) => {
 
-        const SICPBBox = document.getElementById(from).getBoundingClientRect();
+        const fromBBox = document.getElementById(from).getBoundingClientRect();
         const pt1 = [
-            SICPBBox.left + canvas.offsetLeft,
-            (SICPBBox.bottom - SICPBBox.top) / 2 + SICPBBox.top - 12.5 - pointSize/2
+            fromBBox.left + canvas.offsetLeft,
+            (fromBBox.bottom - fromBBox.top) / 2 + fromBBox.top - 12.5 - pointSize/2 + window.pageXOffset
         ];
 
-        const CGBBox = document.getElementById(to).getBoundingClientRect();
+        const toBBox = document.getElementById(to).getBoundingClientRect();
         const pt2 = [
-            CGBBox.left + canvas.offsetLeft,
-            (CGBBox.bottom - CGBBox.top) / 2 + CGBBox.top - 15 /*??*/
+            toBBox.left + canvas.offsetLeft,
+            (toBBox.bottom - toBBox.top) / 2 + toBBox.top - 15 /*??*/ + window.pageXOffset
         ];
         drawPointAt(ctx, pt1[0], pt1[1], 5, color); // starting point only (end = arrow)
         
@@ -279,13 +341,13 @@ function updateArrows() {
             if(i + 1 === nbCurvePoints) return; // last point
             drawLine(ctx, point[0], point[1], curvePoints[i+1][0], curvePoints[i+1][1], 2, color);
         });
-            
+        
+        // final end : arrow
         drawArrow(ctx, curvePoints[nbCurvePoints-2][0], curvePoints[nbCurvePoints-2][1], curvePoints[nbCurvePoints-1][0], curvePoints[nbCurvePoints-1][1], color, 2 /*width*/, /*head_len*/ 10);
 
-
     });
-
 }
+
 
 // TODO
 // compute "true" progression using pages
@@ -311,9 +373,10 @@ function updateProgressBar(book) {
     document.querySelector("#"+bookItem.id+" .progress-value").innerText = `${value} %`;
 }
 
-function redraw() {
-    // get all books
-    const books = getBookList();
+
+
+function redraw(books) {
+
     // get html structure
     const booksHtml = books.map(book => createHtml(book))
     // add it to the DOM
@@ -322,14 +385,17 @@ function redraw() {
     // UI: on clicks, ...
     addEvents();
 
-    // update initial progress
+    // update initial progress (if present in localStorage ?!)
     const bookProgress = JSON.parse(localStorage.getItem('book_progress') ?? '[]');
     bookProgress.forEach(b => {
         const book = getProgress(b.id);
-        updateProgressBar(book)
+        updateProgressBar(book);
     });
 
-    updateArrows();
+    updateArrows(); // to keep them at their position
+
+    searchEl.focus();
 }
+
 
 main();
